3e1b8d82eedddd6aed7931315e70c7143135e830
Brandon Collette <brandoncollette24@gmail.com>
2020-08-19 16:20:00 -0700

    updateGrades method loops through grades parameter and updates the table

--- components/grade-table.js
+++ components/grade-table.js
@@ -4,5 +4,24 @@ class GradeTable {
     }
     updateGrades(grades){
         console.log('grades: ',grades);
+        var tbody = this.tableElement.querySelector('tbody');
+        tbody.textContent = '';
+
+        for(var i = 0;i<grades.length;i++){
+            var tr = document.createElement('tr');
+
+            var td1 = document.createElement("td");
+            td1.textContent = grades[i].name;
+
+            var td2 = document.createElement('td');
+            td2.textContent = grades[i].course;
+
+            var td3 = document.createElement('td');
+            td3.textContent = grades[i].grade;
+
+            tr.append(td1,td2,td3);
+            tbody.append(tr);
+
+        }
     }
 }
