5ad1cee9ab12fe64444ed5dcb87731dc762cd079
Brandon Collette <brandoncollette24@gmail.com>
2020-08-23 11:55:11 -0700

    forms input values print to the console

--- components/app.js
+++ components/app.js
@@ -5,6 +5,9 @@ class App {
         this.gradeForm = gradeForm;
         this.handleGetGradesError = this.handleGetGradesError.bind(this);
         this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+        this.createGrade = this.createGrade.bind(this);
+        this.handleCreateGradeError = this.handleCreateGradeError.bind(this);
+        this.handleCreateGradeSuccess = this.handleCreateGradeSuccess.bind(this);
     }
     handleGetGradesError(error){
         console.error('error: ',error);
@@ -28,6 +31,17 @@ class App {
         });
     }
     start(){
+        this.getGrades();
+        this.gradeForm.onSubmit(this.createGrade);
+    }
+    createGrade(name,course,grade){
+        console.log('name: ',name,' course: ',course,' grade: ',grade);
+
+    }
+    handleCreateGradeError(error){
+        console.error('error: ',error);
+    }
+    handleCreateGradeSuccess(){
         this.getGrades();
     }
 }
