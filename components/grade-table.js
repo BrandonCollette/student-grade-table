3b82f9ade186bd0f58ec3addae814520c94dd89a
Brandon Collette <brandoncollette24@gmail.com>
2020-08-27 15:00:00 -0700

    displays styled delete button

--- components/grade-table.js
+++ components/grade-table.js
@@ -2,24 +2,40 @@ class GradeTable {
     constructor(tableElement){
         this.tableElement = tableElement;
     }
+
     updateGrades(grades){
         console.log('grades: ',grades);
         var tbody = this.tableElement.querySelector('tbody');
         tbody.textContent = '';
+        this.renderGradeRow(grades,this.deleteGrade);
 
-        for(var i = 0;i<grades.length;i++){
+    }
+
+    onDeleteClick(deleteGrade){
+        this.deleteGrade = deleteGrade;
+    }
+
+    renderGradeRow(data,deleteGrade){
+        var tbody = this.tableElement.querySelector('tbody');
+        for(var i = 0;i<data.length;i++){
             var tr = document.createElement('tr');
 
             var td1 = document.createElement("td");
-            td1.textContent = grades[i].name;
+            td1.textContent = data[i].name;
 
             var td2 = document.createElement('td');
-            td2.textContent = grades[i].course;
+            td2.textContent = data[i].course;
 
             var td3 = document.createElement('td');
-            td3.textContent = grades[i].grade;
+            td3.textContent = data[i].grade;
 
-            tr.append(td1,td2,td3);
+            var td4 = document.createElement('td');
+            var button = document.createElement('button');
+            button.textContent = "Delete";
+            button.className = 'btn btn-danger';
+            td4.append(button);
+
+            tr.append(td1,td2,td3,td4);
             tbody.append(tr);
 
         }
